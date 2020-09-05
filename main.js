23dee6670236b3873b7253fbae6ab1aa755eb178
Phillip Caudell <phill@caudell.co.uk>
2020-09-05 18:53:24 +0100

    Updates sheet name

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
 })
 
 function ready() {
-	let url = 'https://api.sheety.co/phill/cluckerRestaurant/menu';
+	let url = 'https://api.sheety.co/phill/cluckerRestaurant/menuItems';
 	fetch(url)
 	.then((response) => response.json())
 	.then(json => {
 		// Group menu items by their category
-		let groupedMenu = _.groupBy(json.menus, 'category');
+		let groupedMenu = _.groupBy(json.menuItems, 'category');
 		// Create a Handlebars template to render items
 		var template = Handlebars.compile(document.getElementById("menu-template").innerHTML);
 		// Render items into Handlebars template, and set the html of the container element
